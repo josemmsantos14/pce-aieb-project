bc1baa14c8b028016bb71fd2129a2c7b8b76a8c3
José Santos <josemmsantos14@gmail.com>
2023-03-08 18:43:30 +0000

    hihihi

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +16,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h2>
+          <a href="#bottom">Scroll down</a>
+        </h2>
       </header>
     </div>
   );
